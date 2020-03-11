0372aea0172b4263047995b5fed71e6874e2e487
Amey Kusurkar <amey1000@gmail.com>
2020-03-11 22:59:43 +0000

    Implement more opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -32,8 +32,21 @@ impl Cpu {
         let opcode = self.memory[self.pc as usize];
 
         let old_pc = self.pc;
+        print!("{:#06x}: ", self.pc);
 
         match opcode {
+            // TODO: Refactor INC X opcodes
+            0x04 => {
+                // INC B
+                let old = self.regs.b;
+                self.regs.b += 1;
+                self.set_flag(ZERO_FLAG, self.regs.b == 0);
+                self.set_flag(SUBTRACT_FLAG, false);
+                self.set_flag(HALF_CARRY_FLAG, ((old & 0xF) + 1) > 0xF);
+                self.pc += 1;
+
+                println!("INC B");
+            },
             0x0C => {
                 // INC C
                 let old = self.regs.c;
@@ -44,6 +57,26 @@ impl Cpu {
                 self.pc += 1;
 
                 println!("INC C");
+            },
+            0x05 => {
+                // DEC B
+                let old = self.regs.b;
+                self.regs.b -= 1;
+                self.set_flag(ZERO_FLAG, self.regs.b == 0);
+                self.set_flag(SUBTRACT_FLAG, true);
+                self.set_flag(HALF_CARRY_FLAG, (old & 0xF) == 0);
+                self.pc += 1;
+
+                println!("DEC B");
+            },
+            // TODO: Refactor LD X, n opcodes
+            0x06 => {
+                // LD B,n
+                let n = self.memory[self.pc as usize + 1];
+                self.regs.b = n;
+                self.pc += 2;
+
+                println!("LD B, {:02x}", n);
             },
             0x0E => {
                 // LD C,n
@@ -61,6 +94,28 @@ impl Cpu {
                 self.pc += 3;
 
                 println!("LD DE, {:04x}", nn);
+            },
+            0x17 => {
+                // RLA
+                let carry = read_bit(self.regs.f, CARRY_FLAG);
+                let (result, carry) = rotate_left_through_carry(self.regs.a, carry);
+                self.regs.a = result;
+
+                self.reset_flags();
+                // Unlike RL X, ZERO_FLAG is RESET
+                self.set_flag(ZERO_FLAG, false);
+                self.set_flag(CARRY_FLAG, carry);
+                self.pc += 1;
+
+                println!("RLA");
+            },
+            0x1A => {
+                // LD A,(DE)
+                let addr = self.regs.read_de();
+                self.regs.a = self.memory[addr as usize];
+                self.pc += 1;
+
+                println!("LD A, (DE)");
             },
             0x20 => {
                 // JR NZ,n
@@ -107,6 +162,13 @@ impl Cpu {
 
                 println!("LD A, {:02x}", n);
             },
+            0x4F => {
+                // LD C,A
+                self.regs.c = self.regs.a;
+                self.pc += 1;
+
+                println!("LD C, A");
+            },
             0x77 => {
                 // LD (HL),A
                 let addr = self.regs.read_hl();
@@ -124,8 +186,33 @@ impl Cpu {
 
                 println!("XOR A");
             },
+            0xC1 => {
+                // POP BC
+                self.regs.write_bc(self.read_u16(self.sp));
+                self.sp += 2;
+                self.pc += 1;
+
+                println!("POP BC");
+            },
+            0xC5 => {
+                // PUSH BC
+                self.sp -= 2;
+                self.write_u16(self.sp, self.regs.read_bc());
+                self.pc += 1;
+
+                println!("PUSH BC");
+            },
             0xCB => {
                 self.execute_prefixed_instruction();
+            },
+            0xCD => {
+                // CALL nn
+                let nn = self.read_u16(self.pc + 1);
+                self.sp -= 2;
+                self.write_u16(self.sp, nn);
+                self.pc = nn;
+
+                println!("CALL {:04x}", nn);
             },
             0xE0 => {
                 // LDH (n),A
@@ -162,6 +249,19 @@ impl Cpu {
         let opcode = self.memory[self.pc as usize];
 
         match opcode {
+            0x11 => {
+                // RL C
+                let carry = read_bit(self.regs.f, CARRY_FLAG);
+                let (result, carry) = rotate_left_through_carry(self.regs.c, carry);
+                self.regs.c = result;
+
+                self.reset_flags();
+                self.set_flag(ZERO_FLAG, result == 0);
+                self.set_flag(CARRY_FLAG, carry);
+                self.pc += 1;
+
+                println!("RL C");
+            },
             0x7c => {
                 // BIT 7,H
                 let bit = read_bit(self.regs.h, 7);
@@ -177,9 +277,18 @@ impl Cpu {
     }
 
     fn read_u16(&self, addr: u16) -> u16 {
-        let lsb = self.memory[addr as usize] as u16;
-        let msb = self.memory[addr as usize + 1] as u16;
+        let addr = addr as usize;
+        let lsb = self.memory[addr] as u16;
+        let msb = self.memory[addr + 1] as u16;
         (msb << 8) | lsb
+    }
+
+    fn write_u16(&mut self, addr: u16, val: u16) {
+        let lsb = (val & 0x00FF) as u8;
+        let msb = ((val & 0xFF00) >> 8) as u8;
+        let addr = addr as usize;
+        self.memory[addr] = lsb;
+        self.memory[addr + 1] = msb;
     }
 
     fn reset_flags(&mut self) {
@@ -192,6 +301,11 @@ impl Cpu {
 }
 
 impl Registers {
+    fn write_bc(&mut self, val: u16) {
+        self.b = ((val & 0xFF00) >> 8) as u8;
+        self.c = (val & 0x00FF) as u8;
+    }
+
     fn write_de(&mut self, val: u16) {
         self.d = ((val & 0xFF00) >> 8) as u8;
         self.e = (val & 0x00FF) as u8;
@@ -200,6 +314,14 @@ impl Registers {
     fn write_hl(&mut self, val: u16) {
         self.h = ((val & 0xFF00) >> 8) as u8;
         self.l = (val & 0x00FF) as u8;
+    }
+
+    fn read_bc(&self) -> u16 {
+        ((self.b as u16) << 8) | self.c as u16
+    }
+
+    fn read_de(&self) -> u16 {
+        ((self.d as u16) << 8) | self.e as u16
     }
 
     fn read_hl(&self) -> u16 {
@@ -215,4 +337,10 @@ fn set_bit(byte: u8, n: u8, x: bool) -> u8 {
 // Reads the nth bit in `byte`
 fn read_bit(byte: u8, n: u8) -> bool {
     (byte & (1 << n)) > 0
+}
+
+fn rotate_left_through_carry(val: u8, carry: bool) -> (u8, bool) {
+    let new_carry = read_bit(val, 7);
+    let new_val = set_bit(val << 1, 0, carry);
+    (new_val, new_carry)
 }
