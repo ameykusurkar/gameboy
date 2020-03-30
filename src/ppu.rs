6f0760bacf4589ef8f2f010eaefdc640b846e0a7
Amey Kusurkar <amey1000@gmail.com>
2020-03-30 20:33:20 +0100

    Set LCD modes

--- src/ppu.rs
+++ src/ppu.rs
@@ -41,13 +41,60 @@ impl Ppu {
         self.cycles = (self.cycles + 1) % SCANLINE_CYCLES;
         if self.cycles == 0 {
             self.scanline = (self.scanline + 1) % LINES_PER_FRAME;
-            memory.ppu_write(0xFF44, self.scanline as u8);
+            memory.write(0xFF44, self.scanline as u8);
+            if memory.read(0xFF44) == memory.read(0xFF45) {
+                let status = memory.read(0xFF41);
+                memory.write(0xFF41, set_bit(status, 2, true));
+                if read_bit(status, 6) {
+                    Self::set_status_interrupt(memory);
+                }
+            }
 
             if self.scanline == LCD_HEIGHT {
-                let interrupt_flags = memory.ppu_read(0xFF0F);
-                memory.ppu_write(0xFF0F, interrupt_flags | 1);
+                // V BLANK
+                Self::set_lcd_mode(memory, 1);
+                let interrupt_flags = memory.read(0xFF0F);
+                memory.write(0xFF0F, interrupt_flags | 1);
+
+                let status = memory.read(0xFF41);
+                if read_bit(status, 4) {
+                    Self::set_status_interrupt(memory);
+                }
+            } else {
+                // OAM SEARCH
+                Self::set_lcd_mode(memory, 2);
+                let status = memory.read(0xFF41);
+                if read_bit(status, 5) {
+                    Self::set_status_interrupt(memory);
+                }
             }
         }
+
+        if self.scanline < LCD_HEIGHT {
+            if self.cycles == OAM_SEARCH_CYCLES {
+                // PIXEL TRANSFER
+                Self::set_lcd_mode(memory, 3);
+            } else if self.cycles == (OAM_SEARCH_CYCLES + PIXEL_TRANSFER_CYCLES) {
+                // H BLANK
+                Self::set_lcd_mode(memory, 0);
+                let status = memory.read(0xFF41);
+                if read_bit(status, 3) {
+                    Self::set_status_interrupt(memory);
+                }
+            }
+        }
+    }
+
+    fn set_status_interrupt(memory: &mut Memory) {
+        let interrupt_flags = memory.read(0xFF0F);
+        memory.write(0xFF0F, interrupt_flags | 2);
+    }
+
+    fn set_lcd_mode(memory: &mut Memory, mode: u8) {
+        let mut status = memory.read(0xFF41);
+        status = set_bit(status, 0, mode & 0x1 > 0);
+        status = set_bit(status, 1, mode & 0x2 > 0);
+        memory.write(0xFF41, status);
     }
 
     pub fn get_background_map(memory: &[u8]) -> Vec<u8> {
@@ -119,7 +166,7 @@ fn get_tile(memory: &[u8], tile_index: u8) -> &[u8] {
     let start_index = if read_bit(memory[0xFF40], 4) {
         0x8000 + (tile_index as usize * TILE_NUM_BYTES)
     } else {
-        ((0x9000 as i32) + (tile_index as i32)) as usize
+        ((0x9000 as i32) + ((tile_index as i8) as i32 * TILE_NUM_BYTES as i32)) as usize
     };
 
     &memory[start_index..start_index+TILE_NUM_BYTES]
@@ -127,17 +174,17 @@ fn get_tile(memory: &[u8], tile_index: u8) -> &[u8] {
 
 fn get_background_map_memory(memory: &[u8]) -> &[u8] {
     if read_bit(memory[0xFF40], 3) {
-        &memory[0x9C00..0x9FFF]
+        &memory[0x9C00..0xA000]
     } else {
-        &memory[0x9800..0x9BFF]
+        &memory[0x9800..0x9C00]
     }
 }
 
 fn get_window_map_memory(memory: &[u8]) -> &[u8] {
     if read_bit(memory[0xFF40], 6) {
-        &memory[0x9C00..0x9FFF]
+        &memory[0x9C00..0xA000]
     } else {
-        &memory[0x9800..0x9BFF]
+        &memory[0x9800..0x9C00]
     }
 }
 
@@ -153,4 +200,8 @@ fn pixel_map(color_number: u8, palette: u8) -> u8 {
 
 fn read_bit(byte: u8, n: u8) -> bool {
     (byte & (1 << n)) > 0
+}
+
+fn set_bit(byte: u8, n: u8, x: bool) -> u8 {
+    (byte & !(1 << n)) | ((x as u8) << n)
 }
