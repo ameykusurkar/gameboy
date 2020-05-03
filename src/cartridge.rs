321e13b6fd97d13f0f05370c360b2d4575f02844
Amey Kusurkar <amey1000@gmail.com>
2020-05-03 16:08:22 +0100

    Implement MBC3, but without timer

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -27,8 +27,9 @@ impl Cartridge {
         let mbc: Box<dyn Mbc> = match rom[0x147] {
             0x00 => Box::new(NoMbc::default()),
             0x01..=0x03 => Box::new(Mbc1::new()),
-            0x0F..=0x13 => Box::new(Mbc3::new()),
-            _ => unimplemented!("Invalid cartridge type: {:02x}h", rom[0x147]),
+            0x0F | 0x10 => unimplemented!("MBC Timer not implemented!"),
+            0x11..=0x13 => Box::new(Mbc3::new()),
+            _ => unimplemented!("Unimplemented cartridge type: {:02x}h", rom[0x147]),
         };
 
         Cartridge {
@@ -196,54 +197,26 @@ impl Mbc for Mbc1 {
 }
 
 struct Mbc3 {
-    rom_bank_lower_bits: u8,
-    upper_bits: u8,
+    rom_bank: u8,
+    ram_bank: u8,
+    // Also enables the Real Time Clock (RTC) register
     ram_enabled: bool,
-    banking_mode: BankingMode,
 }
 
 impl Mbc3 {
     fn new() -> Self {
         Mbc3 {
-            rom_bank_lower_bits: 1,
-            upper_bits: 0,
+            rom_bank: 1,
+            ram_bank: 0,
             ram_enabled: false,
-            banking_mode: BankingMode::ROM,
         }
     }
 
     fn get_real_ram_addr(&self, ram: &[u8], addr: u16) -> Option<usize> {
-        if ram.is_empty() {
-            return None;
-        }
+        if ram.is_empty() { return None; }
 
-        let ram_bank = match self.banking_mode {
-            BankingMode::ROM => 0,
-            BankingMode::RAM => self.upper_bits as usize,
-        };
-
-        let real_addr = (ram_bank << 13 | (addr as usize) & 0x1FFF) % ram.len();
+        let real_addr = ((self.ram_bank as usize) << 13 | (addr as usize) & 0x1FFF) % ram.len();
         Some(real_addr)
-    }
-
-    fn get_bank0_rom_addr(&self, rom: &[u8], addr: u16) -> usize {
-        let upper_bits = match self.banking_mode {
-            BankingMode::ROM => 0,
-            BankingMode::RAM => self.upper_bits as usize,
-        };
-
-        let rom_bank = upper_bits << 5;
-        self.build_rom_addr(rom, rom_bank, addr)
-    }
-
-    fn get_bank1_rom_addr(&self, rom: &[u8], addr: u16) -> usize {
-        let upper_bits = match self.banking_mode {
-            BankingMode::ROM => self.upper_bits as usize,
-            BankingMode::RAM => 0,
-        };
-
-        let rom_bank = upper_bits << 5 | (self.rom_bank_lower_bits as usize);
-        self.build_rom_addr(rom, rom_bank, addr)
     }
 
     fn build_rom_addr(&self, rom: &[u8], rom_bank: usize, addr: u16) -> usize {
@@ -256,10 +229,12 @@ impl Mbc for Mbc3 {
     fn read_rom(&self, rom: &[u8], addr: u16) -> u8 {
         match addr {
             0x0000..=0x3FFF => {
-                rom[self.get_bank0_rom_addr(rom, addr)]
+                let rom_addr = self.build_rom_addr(rom, 0, addr);
+                rom[rom_addr]
             },
             0x4000..=0x7FFF => {
-                rom[self.get_bank1_rom_addr(rom, addr)]
+                let rom_addr = self.build_rom_addr(rom, self.rom_bank as usize, addr);
+                rom[rom_addr]
             },
             _ => unreachable!("Invalid rom read address: {:04x}", addr),
         }
@@ -271,18 +246,18 @@ impl Mbc for Mbc3 {
                 self.ram_enabled = (byte & 0x0F) == 0x0A;
             },
             0x2000..=0x3FFF => {
-                let byte = byte & 0b0001_1111;
-                self.rom_bank_lower_bits = if byte == 0 { 1 } else { byte };
+                let byte = byte & 0b0111_1111;
+                self.rom_bank = if byte == 0 { 1 } else { byte };
             },
             0x4000..=0x5FFF => {
-                self.upper_bits = byte & 0b000_0011;
+                match byte {
+                    0x00..=0x03 => self.ram_bank = byte,
+                    0x08..=0x0C => unimplemented!("Yet to implement RTC registers!"),
+                    _ => unreachable!("Invalid RAM bank/RTC select: {:02x}", byte),
+                }
             },
             0x6000..=0x7FFF => {
-                self.banking_mode = match byte & 0b0000_0001 {
-                    0 => BankingMode::ROM,
-                    1 => BankingMode::RAM,
-                    _ => unreachable!("Invalid cartridge banking mode: {:04x}", byte),
-                }
+                // TODO: Latches clock data, RTC registers yet to be implemented
             },
             _ => unreachable!("Invalid rom write address: {:04x}", addr),
         }
